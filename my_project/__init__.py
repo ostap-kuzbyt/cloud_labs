28d3f3bd4d3d41ab881b03ce0de023378b4e9161
Ostap <ostkuzbyt@gmail.com>
2025-12-14 20:17:18 +0200

    feat: test ci/cd

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -296,7 +296,7 @@ def _init_swagger(app: Flask) -> None:
     api = Api(
         app, 
         title='Pizza Delivery Management API 228',
-        description='Hi',
+        description='Hi there',
         version='2.0',
         doc='/api/docs/',
         prefix='/api/v1',
